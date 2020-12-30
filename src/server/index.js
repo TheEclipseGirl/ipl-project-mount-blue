acb7f79aa239a89d62ef3106ccb9c02cc15f9560
Shalini <shalinibvp.02@gmail.com>
2020-12-30 10:36:56 +0530

    Directory structure setup

---
+++ src/server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 8000;
+
+app.listen(port, (err)=> {
+    if(err){
+        console.log(`Error in running server on port: ${port}`);
+        return;
+    }
+    console.log(`Server is up and running at port: ${port}`);
+});
