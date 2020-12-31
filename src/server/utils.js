a5439c06113a2692198f04e312b3204e0e1a15d3
Shalini <shalinibvp.02@gmail.com>
2020-12-31 12:17:46 +0530

    new webapp branch created

--- src/server/utils.js
+++ src/server/utils.js
@@ -15,11 +15,6 @@ module.exports.createNewFile = (filename, content) => {
 
 module.exports.convertCsvToJson = async (filename) => {
     const file_path= path.join(__dirname + `/../data/${filename}`);
-    // csv()
-    // .fromFile(file_path)
-    // .then((jsonObj)=>{
-    //     return jsonObj;
-    // });
     const jsonArray=await csv().fromFile(file_path);
     return jsonArray;
 }
