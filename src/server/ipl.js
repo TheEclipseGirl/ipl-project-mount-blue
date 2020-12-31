781b4f0f8d2cc521b584f5d498007901bb46088c
Shalini <shalinibvp.02@gmail.com>
2020-12-31 11:28:13 +0530

    changed variable as bowlersInyr

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -80,7 +80,7 @@ module.exports.extraRunsConcededInAYear = (matches , deliveries, year)=>{
 module.exports.topEconomicBowlerInAYear = (matches , deliveries, year, totalRanks) => {
     let ans={};
     let matchId = {};
-    let bowlersIn2015 = {};
+    let bowlersInYr = {};
     let economy = [];
     matches.map((i)=>{
         if(i.season === year){
@@ -89,28 +89,28 @@ module.exports.topEconomicBowlerInAYear = (matches , deliveries, year, totalRank
     });
     deliveries.map((i) => {
         if(matchId[i.match_id]){
-            if(!bowlersIn2015[i.bowler]){
-                bowlersIn2015[i.bowler] = {};
+            if(!bowlersInYr[i.bowler]){
+                bowlersInYr[i.bowler] = {};
                 if(i.wide_runs === '0' && i.noball_runs === '0' && i.penalty_runs == '0'){
-                    bowlersIn2015[i.bowler]['totalBowls'] = '1';
+                    bowlersInYr[i.bowler]['totalBowls'] = '1';
                 }else{
-                    bowlersIn2015[i.bowler]['totalBowls'] = '0';
+                    bowlersInYr[i.bowler]['totalBowls'] = '0';
                 }
-                bowlersIn2015[i.bowler]['totalRunsGiven'] = i.total_runs;
+                bowlersInYr[i.bowler]['totalRunsGiven'] = i.total_runs;
             }else{
                 if(i.wide_runs === '0' && i.noball_runs === '0' && i.penalty_runs == '0'){
-                    bowlersIn2015[i.bowler]['totalBowls'] = (parseInt(bowlersIn2015[i.bowler]['totalBowls']) + 1).toString();
+                    bowlersInYr[i.bowler]['totalBowls'] = (parseInt(bowlersInYr[i.bowler]['totalBowls']) + 1).toString();
                 }
-                bowlersIn2015[i.bowler]['totalRunsGiven'] += (parseInt(bowlersIn2015[i.bowler]['totalRunsGiven']) + parseInt(i.total_runs)).toString;
+                bowlersInYr[i.bowler]['totalRunsGiven'] += (parseInt(bowlersInYr[i.bowler]['totalRunsGiven']) + parseInt(i.total_runs)).toString;
             }
         }
     });
 
-    for(let i in bowlersIn2015){
-        bowlersIn2015[i]['totalOvers'] = (parseInt(bowlersIn2015[i].totalBowls) / 6).toString();
+    for(let i in bowlersInYr){
+        bowlersInYr[i]['totalOvers'] = (parseInt(bowlersInYr[i].totalBowls) / 6).toString();
         let bowlerWithEco = {
             'name': i,
-            'economy': (parseInt(bowlersIn2015[i]['totalRunsGiven']) / parseInt(bowlersIn2015[i]['totalOvers'])).toString()
+            'economy': (parseInt(bowlersInYr[i]['totalRunsGiven']) / parseInt(bowlersInYr[i]['totalOvers'])).toString()
         }
         economy.push(bowlerWithEco);
     }
